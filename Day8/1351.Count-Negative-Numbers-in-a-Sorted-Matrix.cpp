3ecb7cc84fef92ca1eb3518c88241727dcea982b
Michael Ehab <michaelehab16@gmail.com>
2022-04-22 16:01:02 +0200

    Added Day 8 Solution 1/2

---
+++ Day8/1351.Count-Negative-Numbers-in-a-Sorted-Matrix.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int countNegatives(vector<vector<int>>& grid) {
+        int i = 0, j = grid[0].size() - 1;
+        int count = 0, size = grid.size();
+        
+        while(i >= 0 && i < grid.size() && j >= 0 && j < grid[0].size()){
+            cout << grid[i][j] << ' ' << count << endl;
+            if(grid[i][j] < 0){
+                count += size;
+                j--;
+            }
+            else{
+            	i++;
+            	size--;
+            }
+            
+        }
+        return count;
+    }
+};
